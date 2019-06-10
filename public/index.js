a4de3ca2b66f56f92855ccac77602e0d04ec88f0
Alex Lau <alex@tecky.io>
2019-06-10 23:19:22 +0800

    Added month names

--- public/index.js
+++ public/index.js
@@ -6,7 +6,7 @@ const axisHeight = cellSize / 2
 const labelPadding = 5
 const rectRadius = 3
 const monthNameHeight = 40
-const monthNames = ['一月', '二月', '三月', '四月', '五月']
+const monthNames = ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月']
 let thresold = 0;
 
 const statLabelSize = 100
